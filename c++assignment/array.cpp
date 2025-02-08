2a4b1e1778ade1776d6f3d7b6f9e90cfb51e4c3f
nishtha318 <24csu352@ncuindia.edu>
2025-02-08 14:47:44 +0530

    Add files via upload

---
+++ c++assignment/array.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+using namespace std;
+
+void reverse_arr(int arr[] , int n);
+void find_max(int arr[] , int n);
+void find_min(int arr[] , int n);
+
+int main()
+{
+    int n ;
+    cout << "enter number of elements in array\n";
+    cin >> n;
+
+    //array of size n 
+    int arr[n];
+
+    cout << "enter" << " " << n << " " << "elements of the array\n";
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> arr[i];
+    }
+
+    reverse_arr(arr , n);
+    find_max(arr , n);
+    find_min(arr , n);
+     
+}
+
+void reverse_arr(int arr[] , int n)
+{
+    for(int i = 0 ; i <= n / 2 ; i++)
+    {
+        int first = arr[i];
+        int last = arr[n - 1 - i];
+        arr[i] = last;
+        arr[n - 1 - i] = first;
+
+    }
+    
+    cout << "reversed array is :" << " " ;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout << arr[i] << " " ;
+    }
+    cout << endl;
+}
+
+void find_max(int arr[] , int n)
+{
+    int largest , second_largest ;
+
+    if(arr[0] > arr[1])
+    {
+         largest = arr[0];
+         second_largest = arr[1];
+    }
+
+    for(int i = 2 ; i < n ; i++)
+    {
+        if(arr[i] > largest)
+        {
+            second_largest = largest ;
+            largest = arr[i];
+        }
+        
+    }
+    cout << "second largest element in the array is :" <<" " << second_largest << endl ;
+   
+
+}
+
+void find_min(int arr[] , int n)
+{
+    int minimum , second_minimum ;
+
+    if(arr[0] < arr[1])
+    {
+         minimum = arr[0];
+         second_minimum = arr[1];
+    }
+
+    for(int i = 2 ; i < n ; i++)
+    {
+        if(arr[i] < minimum)
+        {
+            second_minimum = minimum;
+            minimum = arr[i];
+        }
+        
+    }
+    cout << "second minimum element in the array is :" <<" " << second_minimum ;
+   
+}
+
+
+
